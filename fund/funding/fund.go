c845c1ee0500ae4907a53d27716fc02d1bae3c6b
Carlos Navas <dante.maniacs@gmail.com>
2020-10-06 08:49:26 +0200

    adding benchmark test to funding project

---
+++ fund/funding/fund.go
@@ -0,0 +1,24 @@
+package funding
+
+// Fund struct
+type Fund struct {
+	// balance is unexported (private)
+	balance int
+}
+
+// NewFund A regular function returning pointer to a fund
+func NewFund(initialBalance int) *Fund {
+	return &Fund{
+		balance: initialBalance,
+	}
+}
+
+// Balance method start with a *receiver*
+func (f *Fund) Balance() int {
+	return f.balance
+}
+
+// Withdraw method
+func (f *Fund) Withdraw(amount int) {
+	f.balance -= amount
+}
