c1ef5e8cad2cafb4d477effbd4ad919065247de8
Carlos Navas <dante.maniacs@gmail.com>
2020-10-07 13:20:50 +0200

    comment for accessing and running postgres container

--- muxServer/main.go
+++ muxServer/main.go
@@ -1,5 +1,14 @@
 package main
 
+/*
+	To be able initialize the databse you have to generate the postgres container
+	with docker. If you don't have docker you can download it by accessing:
+	- https://docs.docker.com/get-docker/
+
+	to create and inicialize the postgres container, execute the next command:
+	# docker run -e POSTGRES_HOST_AUTH_METHOD=trust -it -p 5432:5432 -d postgres
+*/
+
 func main() {
 	a := App{}
 
