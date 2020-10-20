9f7ed1af4e4d6f1dd807f3a25cb0cdaea551ce51
Carlos Navas <dante.maniacs@gmail.com>
2020-10-20 11:57:46 +0200

    example og html render

---
+++ html_template/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+// Todo struct for checking template render
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+// TodoPageData for envolved all variables
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+
+	http.ListenAndServe(":80", nil)
+}
