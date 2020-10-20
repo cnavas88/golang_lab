f0a49d6c7bdfb1dcdd38ef0eac1fede7f304cab0
Carlos Navas <dante.maniacs@gmail.com>
2020-10-20 11:11:31 +0200

    implements get products

--- muxServer/model.go
+++ muxServer/model.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 )
 
 type product struct {
@@ -43,5 +42,25 @@ func (p *product) createProduct(db *sql.DB) error {
 }
 
 func getProducts(db *sql.DB, start int, count int) ([]product, error) {
-	return nil, errors.New("Not implemented")
+	rows, err := db.Query(
+		"SELECT id, name, price FROM products LIMIT $1 OFFSET $2",
+		count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	products := []product{}
+
+	for rows.Next() {
+		var p product
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+
+	return products, nil
 }
