a885116967b53f0e90cd309cd55908273a7af0dd
Carlos Navas <dante.maniacs@gmail.com>
2020-10-28 16:57:03 +0100

    terminate example of session in golang

--- sessions/main.go
+++ sessions/main.go
@@ -16,22 +16,33 @@ var (
 func login(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "cookie-name")
 
+	// Authentication goes here
+	// ...
+
+	// Set user as authenticated
+	session.Values["authenticated"] = true
+	session.Save(r, w)
+}
+
+func secret(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
 	// Check if user is authenticated
 	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
 		http.Error(w, "Forbidden", http.StatusForbidden)
 		return
 	}
 
-	// Print the secret message
+	// Print secret message
 	fmt.Fprintln(w, "The cake is a lie!")
 }
 
-func secret(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func logout(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
 
+	// Revoke user authentication
+	session.Values["authenticated"] = false
+	session.Save(r, w)
 }
 
 func main() {
