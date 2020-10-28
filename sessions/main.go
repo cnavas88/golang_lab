3af37d905b3fdeeaaf5d1707b4f1ed20dee85785
Carlos Navas <dante.maniacs@gmail.com>
2020-10-28 16:53:42 +0100

    add project sessions

---
+++ sessions/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/sessions"
+)
+
+var (
+	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
+	key   = []byte("super-secret-key")
+	store = sessions.NewCookieStore(key)
+)
+
+func login(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func secret(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	http.HandleFunc("/secret", secret)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+
+	http.ListenAndServe(":8080", nil)
+}
